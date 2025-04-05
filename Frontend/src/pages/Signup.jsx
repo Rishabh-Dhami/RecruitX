9066b1213b743d5b85d09b11078e17e68306f7f9
Rishabh Singh <rishabhdhami090@gmail.com>
2025-04-05 09:21:32 +0530

    fix signup page

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -171,7 +171,6 @@ function Signup() {
               <option value="">--Select Role--</option>
               <option value="recruiter">Recruiter</option>
               <option value="candidate">Candidate</option>
-              <option value="admin">Admin</option>
             </select>
           </div>
           <div className="mb-4 w-full">
